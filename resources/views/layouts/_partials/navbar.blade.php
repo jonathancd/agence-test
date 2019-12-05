993a849f0db28fc279dd5a2903441e6b5c693614
Jonathan <jonathancuotto@gmail.com>
2019-12-05 17:22:17 -0400

    create navbar

---
+++ resources/views/layouts/_partials/navbar.blade.php
@@ -0,0 +1,59 @@
+<nav class="navbar navbar-icon-top navbar-expand-lg navbar-expand-md navbar-dark bg-dark">
+  
+  <a class="navbar-brand" href="#">
+    <img src="{{ asset('images/agence.png') }}" alt="">
+  </a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-menu" aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbar-menu">
+
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="fa fa-home"></i>
+          Agence
+          </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="fa fa-clipboard">
+          </i>
+          Projetos
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="fa fa-folder-open">
+          </i>
+          Administrativo
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="fa fa-building">
+          </i>
+          Comercial
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="fa fa-money">
+          </i>
+          Financiero
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="fa fa-user">
+          </i>
+          Usuario
+        </a>
+      </li>
+    </ul>
+
+    <a class="text-white btn-logout btn-fl-xs" href="#">Cerrar sesión</a>
+  </div>
+</nav>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+	
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+	
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+	<div id="container">
+		@include('layouts._partials.navbar')
+	</div>
+
+	
+</body>
+</html>
